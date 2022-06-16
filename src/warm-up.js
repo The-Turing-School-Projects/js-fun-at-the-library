4dbe577cfd69c08cf763a9ac2dd2fa24f27b6291
Sebastian Black <sebbyvonblack@gmail.com>
2022-06-15 21:27:06 -0500

    Completed Warm Up file

--- src/warm-up.js
+++ src/warm-up.js
@@ -13,27 +13,29 @@
 // var title = "Harry Potter";
 // var ...
 
-
+var title = "Book Title";
+var pages = 240;
 
 // Reassign (not initialize) your "pages" variable from above. An editor came around and
 // condensed the book, use a built in math operator to make the page count a quarter
 // of what it currently is 
 
-
+pages = 240 / 4;
 
 // Initialize a new variable "movie", but leave it undefined
 
-
+var movie = "";
 
 // Create a variable named "sequel", and assign it to a be a sequel of your
 // book (made up or not!). Your sequel must make use of your "title" variable from above
 
+var sequel = title + " part two"
 
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the book you chose is your favorite book or not
 
-
+var isFavorite = true;
 
 
 // Create a new variable named "isShortStory", and use a comparison operator to
@@ -41,7 +43,13 @@
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
 
+var isShortStory = undefined;
 
+if (pages < 25) {
+     isShortStory = true;
+} else {
+     isShortStory = false;
+}
 
 //------------------------- Conditionals -------------------------//
 
@@ -50,13 +58,20 @@
 // the statement 'I could read that in a day!', otherwise, log the statement 
 // 'Ok, maybe I could read that in a day and a half'
 
-
+if (pages < 1000) {
+    console.log("I could read that in a day!");
+} else {
+    console.log("Ok, maybe I could read that in a day and a half");
+}
 
 // Express the following in code: If the variable "isFavorite" is true,
 // log the statement 'My favorite book is [title]!'. Otherwise, log the
 // statement 'I recently read the [title] book'. In either case, the value stored in
 // the "title" variable should be included in the logged statement.
 
+if (isFavorite == true) {
+    console.log(`My favorite book is ${title}`);
+}
 
 
 //------------------------- Arrays -------------------------//
@@ -64,18 +79,19 @@
 
 // Create a new variable "books" and assign it to an Array of 5 different
 // book titles, represented by their names as Strings.
+var books = ["bookOne","bookTwo","bookThree","bookFour","bookFive"]
 
 
 // Write the code to access the 3rd element of the Array, using bracket
 // notation.
-
+console.log(books[2])
 
 // Create a new variable "belowTwoHundred" and assign it to an Array of 12
 // different numbers below two hundred.
-
+var belowTwoHundred = [1,34,54,65,87,101,134,154,167,172,188,195];
 
 // Write the code to access the 5th element of the array, using bracket notation.
-
+console.log(belowTwoHundred[4])
 
 //------------------------- Objects -------------------------//
 
@@ -83,6 +99,13 @@
 // keys: name, and hasThesaurus. The value for name should the string 'Webster's', and
 // the value for hasThesaurus should be false
 
+var dictionary = {
+    name: "Webster's",
+    hasThesaurus: false,
+};
 
 // Write the code to add the key of pageCount, with the value of 720. Use dot
 // notation to do this. DO NOT modify your code from above.
+dictionary.pageCount = 720;
+
+console.log(dictionary)
