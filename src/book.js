717e34210bead37f1a0a3ceb5344a60024af4f3b
Sebastian Black <sebbyvonblack@gmail.com>
2022-06-15 22:20:11 -0500

    Finished books.js problems

--- src/book.js
+++ src/book.js
@@ -1,12 +1,45 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return "The " + title
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  }
+}
+
+function saveReview(title, reviews) {
+  if (reviews.includes(title)) {
+    return
+  } else {
+    return (reviews.push(title));
+    }
+}
+
+function calculatePageCount(title) {
+  return title.length * 20;
+}
+
+function writeBook(bookTitle, bookCharacter, genre) {
+  return {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: genre,
+  }
+}
+
+function editBook(pageCount) {
+  pageCount.pageCount = pageCount.pageCount * .75
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+   buildMainCharacter,
+   saveReview,
+   calculatePageCount,
+   writeBook,
+   editBook
 }
